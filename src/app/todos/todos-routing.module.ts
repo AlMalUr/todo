01fcfa8c63360afb908c88d38f8003cfd1aeeaa2
AleksandrMaltsev <alex.m@2muchcoffee.com>
2018-11-12 11:08:34 +0200

    second commit

---
+++ src/app/todos/todos-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from './todos.component';
+
+
+const routes: Routes = [
+  {path: '',
+    component: TodosComponent,
+  children: [
+    {path: '', loadChildren: './all-items/all-items.module#AllItemsModule'},
+    {path: 'active', loadChildren: './active-items/active-items.module#ActiveItemsModule'},
+    {path: 'completed', loadChildren: './completed-items/completed-items.module#CompletedItemsModule'}
+  ]},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TodosRoutingModule { }
---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodosRoutingModule } from './todos-routing.module';
+
+import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
+import {AllItemsModule} from './all-items/all-items.module';
+import {CompletedItemsModule} from './completed-items/completed-items.module';
+import {ActiveItemsModule} from './active-items/active-items.module';
+
+import { TodosComponent } from './todos.component';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TodosRoutingModule,
+    HeaderModule,
+    FooterModule,
+    AllItemsModule,
+    CompletedItemsModule,
+    ActiveItemsModule
+  ],
+  declarations: [TodosComponent]
+})
+export class TodosModule { }
