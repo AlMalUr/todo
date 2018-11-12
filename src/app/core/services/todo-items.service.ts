8f947d9cadf010e1692b1cd43c7e55e661273389
AleksandrMaltsev <alex.m@2muchcoffee.com>
2018-11-12 15:04:51 +0200

    build: milestone12
    receive and output todoItems titles
    
    & some style changes

--- src/app/core/services/todo-items.service.ts
+++ src/app/core/services/todo-items.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+import {TODO_ITEMS} from '../mock/mock-todo-items';
+import {TodoItem} from '../models/todo-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoItemsService {
+  todoItems: TodoItem[] = TODO_ITEMS;
 }
--- src/app/todos/all-items/all-items.component.ts
+++ src/app/todos/all-items/all-items.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {TodoItem} from '../../core/models/todo-item';
+import {TodoItemsService} from '../../core/services/todo-items.service';
 
 @Component({
   selector: 'app-all-items',
   templateUrl: './all-items.component.html',
   styleUrls: ['./all-items.component.scss']
 })
-export class AllItemsComponent {
+export class AllItemsComponent implements OnInit {
+  todoItems: TodoItem[];
+  constructor (private todoItemService: TodoItemsService) {
+  }
+  ngOnInit() {
+    this.todoItems = this.todoItemService.todoItems;
+  }
 }
