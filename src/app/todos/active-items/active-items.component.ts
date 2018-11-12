30e934b22b6873f7bd98af1c4d336153fe0da685
AleksandrMaltsev <alex.m@2muchcoffee.com>
2018-11-12 13:08:16 +0200

    refactor: remove unused modules imports & split styles

--- src/app/todos/active-items/active-items.component.ts
+++ src/app/todos/active-items/active-items.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-active-items',
   templateUrl: './active-items.component.html',
   styleUrls: ['./active-items.component.scss']
 })
-export class ActiveItemsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ActiveItemsComponent {
 }
--- src/app/todos/footer/footer.component.ts
+++ src/app/todos/footer/footer.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: [
-    './footer.component.scss',
-    '../todos.component.scss'
+    './footer.component.scss'
   ]
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FooterComponent {
 }
