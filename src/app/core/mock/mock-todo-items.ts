94995523e0b7260cacbeb7106cc5398f2f5fa958
AleksandrMaltsev <alex.m@2muchcoffee.com>
2018-11-20 12:59:44 +0200

    refactor(src/app/core/mock/mock-todo-items.ts): delete mock data

--- src/app/core/mock/mock-todo-items.ts
+++
@@ -1,9 +0,0 @@
-import {TodoItem} from '../models/todo-item';
-
-export const TODO_ITEMS: TodoItem[] = [
-  {id: 1, title: 'aaaaaaaa', complete: false},
-  {id: 2, title: 'bbbbbbbb', complete: true},
-  {id: 3, title: 'cccccccc', complete: false},
-  {id: 4, title: 'dddddddd', complete: true},
-  {id: 5, title: 'eeeeeeee', complete: false}
-];
